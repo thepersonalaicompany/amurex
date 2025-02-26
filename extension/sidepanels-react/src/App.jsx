1a5cea58ea7232f9da71d8a4c21ca4003904478d
Raghav Dhingra <raghav.dhingra15@gmail.com>
2025-02-26 23:38:15 +0530

    Updated sidepanel

--- extension/sidepanels-react/src/App.jsx
+++ extension/sidepanels-react/src/App.jsx
@@ -1,17 +1,30 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+// import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Sidepanel from "./sidepanel";
 import ChatSidepanel from "./chatsidepanel";
 
 const App = () => {
+  const [route, setRoute] = React.useState("/");
+  const [meetingId, setMeetingId] = React.useState(null);
+
+  console.log("route", route);
+
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Sidepanel />} />
-        <Route path='/chat' element={<ChatSidepanel />} />
-      </Routes>
-    </Router>
+    <>
+      {route === "/" && (
+        <Sidepanel setRoute={setRoute} setMeetingId={setMeetingId} />
+      )}
+      {route === "/chat" && (
+        <ChatSidepanel setRoute={setRoute} meetingId={meetingId} />
+      )}
+    </>
+    // <Router>
+    //   <Routes>
+    //     <Route path='/' element={<Sidepanel />} />
+    //     <Route path='/chat' element={<ChatSidepanel />} />
+    //   </Routes>
+    // </Router>
   );
 };
 
