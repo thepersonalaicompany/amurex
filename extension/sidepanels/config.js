30ea4545cb1299e97fd59ece33a83fd3fdf3a00f
Sanskar Jethi <sansyrox@gmail.com>
2025-02-26 12:12:17 -0800

    delete boring things

--- extension/sidepanels/config.js
+++
@@ -1,6 +0,0 @@
-const AMUREX_CONFIG = {
-  // there is one more config in the content.js script
-  BASE_URL_BACKEND: "https://api.amurex.ai",
-  BASE_URL_WEB: "https://app.amurex.ai",
-  ANALYTICS_ENABLED: true
-};
